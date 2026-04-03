de7b79df15ad38bf6797a7ed8659aaa901b7bbe5
JvanRd <slayador10@gmail.com>
2026-04-03 15:19:23 -0500

    implementacion terminada de crear, y obtener trazabilidad

--- src/main/java/co/edu/uptc/formales/automatas/presenter/IPresenter.java
+++ src/main/java/co/edu/uptc/formales/automatas/presenter/IPresenter.java
@@ -2,13 +2,9 @@ package co.edu.uptc.formales.automatas.presenter;
 
 public interface IPresenter {
     public void crearAutomata();
-    public void guardarEstados();
-    public void guardarAlfabeto();
-    public void seleccionarEstadoInicial();
-    public void seleccionarEstadosAceptacion();
     public void crearFuncionTransicion();
     public void exportarAutomata();
     public void importarAutomata();
     public void evaluarCadenas();
-    public String ObtenerTrazabilidad(String cadena);
+    public void ObtenerTrazabilidad();
 }
--- src/main/java/co/edu/uptc/formales/automatas/presenter/Presenter.java
+++ src/main/java/co/edu/uptc/formales/automatas/presenter/Presenter.java
@@ -1,39 +1,88 @@
 package co.edu.uptc.formales.automatas.presenter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import co.edu.uptc.formales.automatas.model.Estado;
+import co.edu.uptc.formales.automatas.model.IModel;
 import co.edu.uptc.formales.automatas.model.Simulacion;
+import co.edu.uptc.formales.automatas.model.TipoAutomata;
+import co.edu.uptc.formales.automatas.model.TipoEstado;
+import co.edu.uptc.formales.automatas.view.IView;
+import co.edu.uptc.formales.automatas.view.View;
 
 public class Presenter implements IPresenter{
     
-    private Simulacion simulacion;
+    private IModel simulacion;
+    private IView view;
 
     @Override
     public void crearAutomata() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crearAutomata'");
+        List<String> alfabeto = obtenerAlfabeto();
+        List<String> estados = obtenerEstados();
+        String estadoInicialV = obtenerEstadoInicial();
+        List<String> estadosAceptacionV = obtenerEstadosAceptacion();
+        List<Estado> estadosModel = crearEstados(estados);
+        TipoAutomata tipo = obtenerTipoAutomata();
+        Estado estadoInicial = asignarEstadoInicial(estadosModel, estadoInicialV);
+        List<Estado> estadosAceptacion = asignarEstadosFinales(estadosModel, estadosAceptacionV);
+        simulacion.crearAutomata(estadosModel, alfabeto, estadoInicial, estadosAceptacion, tipo);
     }
 
-    @Override
-    public void guardarEstados() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'guardarEstados'");
+    private List<Estado> crearEstados(List<String> estados) {
+        List<Estado> estadosModel = new ArrayList<>();
+        for (String estadoNombre : estados) {
+            Estado aux = new Estado(estadoNombre, TipoEstado.DEFAULT);
+            estadosModel.add(aux);
+        }
+        return estadosModel;
     }
 
-    @Override
-    public void guardarAlfabeto() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'guardarAlfabeto'");
+    private Estado asignarEstadoInicial(List<Estado> estadosModel, String estadoInicial) {
+        for (Estado estado : estadosModel) {
+            if(estado.getNombre().equals(estadoInicial)){
+                estado.setTipo(TipoEstado.INITIAL);
+                return estado;
+            }
+        }
+        return null;
     }
 
-    @Override
-    public void seleccionarEstadoInicial() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'seleccionarEstadoInicial'");
+    private List<Estado> asignarEstadosFinales(List<Estado> estadosModel, List<String> estadosAceptacion) {
+        List<Estado> estadosAceptacionAux = new ArrayList<>();
+        for (Estado estado : estadosModel) {
+            if(estadosAceptacion.contains(estado.getNombre())){
+                estado.setTipo(TipoEstado.FINAL);
+                estadosAceptacionAux.add(estado);
+            }
+        }
+        return estadosAceptacionAux;
     }
 
-    @Override
-    public void seleccionarEstadosAceptacion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'seleccionarEstadosAceptacion'");
+    private TipoAutomata obtenerTipoAutomata(){
+        TipoAutomata tipo = view.getTipoAutomata();
+        return tipo;
+    }
+
+    public List<String> obtenerEstados() {
+        List<String> estadosV = view.getEstados();
+        return estadosV;
+    }
+
+    public List<String> obtenerAlfabeto() {
+        List<String> alfabetoV = view.getAlfabeto();
+        return alfabetoV;
+    }
+
+    public String obtenerEstadoInicial() {
+        String estadoInicialV = view.getEstadoInicial();
+        return estadoInicialV;
+    }
+
+    public List<String> obtenerEstadosAceptacion() {
+        List<String> estadosApectacionV = view.getEstadosAceptacion();
+        return estadosApectacionV;
     }
 
     @Override
@@ -61,9 +110,14 @@ public class Presenter implements IPresenter{
     }
 
     @Override
-    public String ObtenerTrazabilidad(String cadena) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ObtenerTrazabilidad'");
+    public void ObtenerTrazabilidad() {
+        List<String> cadenaTrazabilidad = view.getCadenaPrueba();
+        HashMap<String, String> resultadosTrazabilidad = new HashMap<>();
+        for (String string : cadenaTrazabilidad) {
+            String trazabilidad = simulacion.obtenerTrazabilidad(string);
+            resultadosTrazabilidad.put(string, trazabilidad);
+        }
+        view.mostrarTrazabilidad(resultadosTrazabilidad);
     }
 
 }
